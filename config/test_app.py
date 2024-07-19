74e385df2dc6d98d256f5f44d3cdb094a6a772d5
crm.c.31904 <crm.c.31904@dz.com>
2024-07-20 00:22:08 +0200

    Updated APP | DB Tests

--- config/test_app.py
+++ config/test_app.py
@@ -3,11 +3,24 @@ from fastapi.testclient import TestClient
 
 from app import app
 
+# Create Test Client of FastAPI
 client = TestClient(app)
 
 
 @pytest.mark.asyncio
 async def test_main_route():
+    """
+    Test for APP
+    """
     response = client.get("/app-status")
     assert response.status_code == 200
     assert response.json() == {"status": "success"}
+
+
+@pytest.mark.asyncio
+async def test_db_route():
+    """
+    Test for DB
+    """
+    response = client.get("/db/status/users")
+    assert response.status_code == 200
