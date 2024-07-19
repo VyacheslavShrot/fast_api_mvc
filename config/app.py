b7642b53f9a18f6c4cb2de42e2e728e8d43f475e
crm.c.31904 <crm.c.31904@dz.com>
2024-07-20 00:21:49 +0200

    Updated APP Conf

--- config/app.py
+++ config/app.py
@@ -1,12 +1,47 @@
 from fastapi import FastAPI
+from sqlalchemy.future import select
 
-app = FastAPI(
+from config.database import database, async_session
+from config.models import users
+
+# Create Web APP FastAPI
+app: FastAPI = FastAPI(
     title="Fast API MVC"
 )
 
 
+@app.on_event("startup")
+async def startup():
+    """
+    Connect To DataBase
+    """
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    """
+    Disconnect To DataBase
+    """
+    await database.disconnect()
+
+
 @app.get("/app-status")
 async def app_status():
+    """
+    Just APP Status
+    """
     return {
         "status": "success"
     }
+
+
+@app.get("/db/status/users", response_model=list[dict])
+async def db_status_users():
+    """
+    Just for DB Status of Checking Successful Created Database and Users Table
+    """
+    async with async_session() as session:
+        query = select(users)
+        result = await session.execute(query)
+        return result.scalars().all()
