beafa9304c689b14bd99f1405f96dd410f7f84c0
crm.c.31904 <crm.c.31904@dz.com>
2024-07-24 01:10:38 +0200

    Updated Models

--- config/models.py
+++ config/models.py
@@ -1,14 +1,24 @@
-from sqlalchemy import Table, Column, Integer, String, MetaData
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.orm import declarative_base
 
 metadata: MetaData = MetaData()
+
+Base = declarative_base()
 
 """
             Models
 """
-users: Table = Table(
-    "users",
-    metadata,
-    Column("id", Integer, primary_key=True, index=True),
-    Column("email", String(50), unique=True),
-    Column("password", String(50)),
-)
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(50), unique=True, index=True)
+    password = Column(String(100))
+
+    def __str__(self):
+        return f"{self.id} {self.email} {self.password}"
+
+    def __repr__(self):
+        return f"<User(id={self.id}, email={self.email})>"
