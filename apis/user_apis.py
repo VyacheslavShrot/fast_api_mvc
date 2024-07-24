366eedbb984d1b0177bf4eac0e392fd667f52458
crm.c.31904 <crm.c.31904@dz.com>
2024-07-25 01:18:40 +0200

    Added Login API

--- apis/user_apis.py
+++ apis/user_apis.py
@@ -8,7 +8,7 @@ from apis.utils.token import create_access_token
 from config.database import async_session
 from config.logger import logger
 from config.models import User
-from config.schemas import SignUp
+from config.schemas import SignUpOrLogin
 
 # Register APIs
 users_router: APIRouter = APIRouter()
@@ -16,7 +16,7 @@ users_router: APIRouter = APIRouter()
 
 @users_router.post("/user/signup")
 async def signup(
-        body: SignUp
+        body: SignUpOrLogin
 ) -> JSONResponse:
     """
     SignUp API for Creating User and JWT Access Token
@@ -78,6 +78,71 @@ async def signup(
         return JSONResponse(
             {
                 "error": f"An error occurred while signup user | {e}"
+            },
+            status_code=500
+        )
+
+
+@users_router.post("/user/login")
+async def login(
+        body: SignUpOrLogin
+) -> JSONResponse:
+    """
+    Login API for Check Input Email and Password and Return JWT Access Token
+    """
+    try:
+        async with async_session() as session:
+            # Get User with Input Email
+            query: Select = select(User).filter_by(email=body.email)
+
+            # Execute Query
+            result: Result = await session.execute(query)
+
+            existing_user: User = result.scalars().first()
+            if not existing_user:
+                return JSONResponse(
+                    {
+                        "error": "Such User with Such Email Are Not Exist"
+                    },
+                    status_code=400
+                )
+
+            # Check Password
+            validated_password: bool = bcrypt.checkpw(
+                password=body.password.encode('utf-8'),
+                hashed_password=existing_user.password.encode('utf-8')
+            )
+            if not validated_password:
+                return JSONResponse(
+                    {
+                        "error": "Invalid Password"
+                    },
+                    status_code=400
+                )
+
+            # Generate JWT Token
+            token: str = create_access_token(
+                {
+                    "user_email": existing_user.email
+                }
+            )
+
+            return JSONResponse(
+                {
+                    "success": True,
+                    "user": {
+                        "id": existing_user.id,
+                        "email": existing_user.email
+                    },
+                    "token": token
+                },
+                status_code=200
+            )
+    except Exception as e:
+        logger.error(f"An error occurred while login user | {e}")
+        return JSONResponse(
+            {
+                "error": f"An error occurred while login user | {e}"
             },
             status_code=500
         )
