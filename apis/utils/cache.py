f6f9bfc5644c62e237e3062dcff44aea3f7c87c5
crm.c.31904 <crm.c.31904@dz.com>
2024-08-02 01:21:53 +0200

    Updated Cache Functionality

--- apis/utils/cache.py
+++ apis/utils/cache.py
@@ -16,6 +16,24 @@ caches.set_config({
 cache = caches.get('default')
 
 
+async def get_cached_posts_and_cache_key(
+        user_email: str
+) -> tuple[
+    list | None,
+    str
+]:
+    """
+    Get Cached Posts And Cache Key by User Email
+    """
+    # Cache Key for Posts
+    cache_key: str = f"{user_email}_posts"
+
+    # Get Cached Posts
+    cached_posts: list | None = await cache.get(cache_key, None)
+
+    return cached_posts, cache_key
+
+
 async def get_and_add_user_posts_into_cache(
         user_email: str,
         post: Post | list = None
@@ -24,11 +42,12 @@ async def get_and_add_user_posts_into_cache(
     Get From Cache And Add Posts Into Cache
     """
 
-    # Cache Key for Posts
-    cache_key: str = f"{user_email}_posts"
-
-    # Get Cached Posts
-    cached_posts: list | None = await cache.get(cache_key, None)
+    # Get Cached Posts and Cache Key
+    cached_posts, cache_key = await get_cached_posts_and_cache_key(
+        user_email=user_email
+    )
+    cached_posts: list | None
+    cache_key: str
 
     # If Post Object Exist
     if post:
@@ -48,11 +67,33 @@ async def get_and_add_user_posts_into_cache(
 
         # If Post = List Object -> Than Mean Called This Function from "Get Posts API"
         if isinstance(post, list):
-
             # Add Into Cached Posts ALL User Posts
             cached_posts: list[dict] = post
 
-        # Get Cached Posts
+        # Update Cache
         await cache.set(cache_key, cached_posts)
 
     return cached_posts
+
+
+async def delete_post_from_cache(
+        user_email: str,
+        post_id: int
+) -> None:
+    """
+    Update Cached Posts List Without Deleted Post
+    """
+
+    # Get Cached Posts and Cache Key
+    cached_posts, cache_key = await get_cached_posts_and_cache_key(
+        user_email=user_email
+    )
+    cached_posts: list | None
+    cache_key: str
+
+    if cached_posts:
+        # Create List with Cached Post Without Specific Post
+        cached_posts: list = [post for post in cached_posts if post["id"] != post_id]
+
+        # Update Cache
+        await cache.set(cache_key, cached_posts)
