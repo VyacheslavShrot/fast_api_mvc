c37468d4b8bbeb4b57d871e35381b383c93da4f9
crm.c.31904 <crm.c.31904@dz.com>
2024-08-02 01:21:35 +0200

    Added New APi -> Post Delete API

--- apis/posts_apis.py
+++ apis/posts_apis.py
@@ -5,12 +5,12 @@ from sqlalchemy import Select, Result
 from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload
 
-from apis.utils.cache import get_and_add_user_posts_into_cache
+from apis.utils.cache import get_and_add_user_posts_into_cache, delete_post_from_cache
 from apis.utils.token import get_current_user_email
 from config.database import async_session
 from config.logger import logger
 from config.models import User, Post
-from config.schemas import PostAdd
+from config.schemas import PostAdd, PostDelete
 
 # Register APIs
 posts_router: APIRouter = APIRouter()
@@ -156,6 +156,104 @@ async def get_posts(
         return JSONResponse(
             {
                 "error": f"An error occurred while get posts | {e}"
+            },
+            status_code=500
+        )
+
+
+@posts_router.post("/post/delete")
+async def delete_post(
+        body: PostDelete,
+        current_user_email: str | bool = Depends(get_current_user_email)
+) -> JSONResponse:
+    """
+    Delete Post API with Auth Token Requireid for Deleting Post
+    """
+    try:
+        async with async_session() as session:
+            if not current_user_email:
+                return JSONResponse(
+                    {
+                        "error": "invalid Token"
+                    },
+                    status_code=401
+                )
+
+            # Get User with Email from Token
+            query: Select = select(User).filter_by(email=current_user_email).options(selectinload(User.posts))
+
+            # Execute Query
+            result: Result = await session.execute(query)
+
+            user: User = result.scalars().first()
+            if not user:
+                return JSONResponse(
+                    {
+                        "error": "Don't Exist Such User with Such Email"
+                    },
+                    status_code=401
+                )
+
+            # Get Post by Input Post Id
+            query: Select = select(Post).filter_by(id=body.post_id)
+
+            # Execute Query
+            result: Result = await session.execute(query)
+
+            post: Post = result.scalars().first()
+            if not post:
+                return JSONResponse(
+                    {
+                        "error": "Don't Exist Such Post with Such Post Id"
+                    },
+                    status_code=400
+                )
+
+            # Get ALL user Posts
+            user_posts: list[Post] = user.posts
+            if not user_posts:
+                return JSONResponse(
+                    {
+                        "error": "Don't Exist Such Post for Such User"
+                    },
+                    status_code=400
+                )
+
+            # Create List of ALL User Posts Ids
+            user_posts_id: list = [user_post.id for user_post in user_posts]
+
+            # Check if That Post is About This User
+            if post.id not in user_posts_id:
+                return JSONResponse(
+                    {
+                        "error": "That Post is Not About This User"
+                    },
+                    status_code=400
+                )
+
+            # Add Post into Session for Deleting
+            await session.delete(post)
+
+            # Delete Post from DB
+            await session.commit()
+
+            # Update Cached Posts Without Deleted Post
+            await delete_post_from_cache(
+                user_email=current_user_email,
+                post_id=post.id
+            )
+
+            return JSONResponse(
+                {
+                    "success": True
+                },
+                status_code=200
+            )
+    except Exception as e:
+        logger.error(f"An error occurred while delete post | {e}")
+        return JSONResponse(
+            {
+                "error": f"An error occurred while delete post | {e}"
             },
             status_code=500
         )
